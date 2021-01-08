10a04fedeadbd6d05c56fd7691b9813f19d7c360
Gee <1176243316@qq.com>
2021-01-08 11:45:52 +0800

    完成选项卡“我”的二维码功能

--- im_bird_sys/src/main/java/com/gee/service/impl/UserServiceImpl.java
+++ im_bird_sys/src/main/java/com/gee/service/impl/UserServiceImpl.java
@@ -3,15 +3,24 @@ package com.gee.service.impl;
 import com.gee.mapper.UserMapper;
 import com.gee.pojo.User;
 import com.gee.service.UserService;
+import com.gee.utils.FastDFSClient;
+import com.gee.utils.FileUtils;
+import com.gee.utils.QRCodeUtils;
 import com.gee.utils.Sid;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Resource
     private UserMapper userMapper;
+    @Resource
+    private QRCodeUtils qrCodeUtils;
+    @Resource
+    private FastDFSClient fastDFSClient;
 
     @Resource
     Sid sid;
@@ -31,6 +40,17 @@ public class UserServiceImpl implements UserService {
     @Override
     public User insert(User user) {
         user.setId(sid.nextShort());
+        //为每一个用户生成唯一二维码
+        String qrCodePath = "D:/user" + user.getId() + "qrcode.png";
+        qrCodeUtils.createQRCode(qrCodePath, "bird_qrcode:" + user.getUsername());
+        MultipartFile qrcodeFile = FileUtils.fileToMultipart(qrCodePath);
+        String qrcodeUrl = null;
+        try {
+            qrcodeUrl = fastDFSClient.uploadQRCode(qrcodeFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        user.setQrcode(qrcodeUrl);
         userMapper.insert(user);
         return user;
     }
